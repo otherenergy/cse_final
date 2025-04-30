c01fb24a4e76e3e0a5c9531be2a8ff74cdce8bab
otherenergy <otherenergy@hotmail.com>
2025-05-01 01:47:33 +0200

    Contact Form

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,200 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 2888;
+exports.ids = [2888];
+exports.modules = {
+
+/***/ 1083:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _app)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(6689);
+// EXTERNAL MODULE: external "next/script"
+var script_ = __webpack_require__(4780);
+var script_default = /*#__PURE__*/__webpack_require__.n(script_);
+// EXTERNAL MODULE: external "next/head"
+var head_ = __webpack_require__(968);
+var head_default = /*#__PURE__*/__webpack_require__.n(head_);
+;// CONCATENATED MODULE: ./src/common/scrollToTop.js
+const scrollToTop = ()=>{
+    let offset = 150;
+    let progressWrap = document.querySelector(".progress-wrap");
+    let progressPath = document.querySelector(".progress-wrap path");
+    let pathLength = progressPath.getTotalLength();
+    const updateProgress = ()=>{
+        let scroll = window.pageYOffset;
+        let height = document.documentElement.scrollHeight - window.innerHeight;
+        let progress = pathLength - scroll * pathLength / height;
+        progressPath.style.strokeDashoffset = progress;
+    };
+    if (progressWrap) {
+        progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+        progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = "stroke-dashoffset 10ms linear";
+        updateProgress();
+        window.addEventListener("scroll", updateProgress);
+        window.addEventListener("scroll", function() {
+            if (window.pageYOffset > offset) {
+                progressWrap.classList.add("active-progress");
+            } else {
+                document.querySelector(".progress-wrap").classList.remove("active-progress");
+            }
+        });
+        progressWrap.addEventListener("click", function(event) {
+            event.preventDefault();
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+            return false;
+        });
+    }
+};
+/* harmony default export */ const common_scrollToTop = (scrollToTop);
+
+;// CONCATENATED MODULE: ./src/components/Progress-Scroll/index.jsx
+
+
+
+const ProgressScroll = ()=>{
+    (0,external_react_.useEffect)(()=>{
+        common_scrollToTop();
+    }, []);
+    return /*#__PURE__*/ jsx_runtime_.jsx("div", {
+        className: "progress-wrap cursor-pointer",
+        children: /*#__PURE__*/ jsx_runtime_.jsx("svg", {
+            className: "progress-circle svg-content",
+            width: "100%",
+            height: "100%",
+            viewBox: "-1 -1 102 102",
+            children: /*#__PURE__*/ jsx_runtime_.jsx("path", {
+                d: "M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
+            })
+        })
+    });
+};
+/* harmony default export */ const Progress_Scroll = (ProgressScroll);
+
+// EXTERNAL MODULE: external "@emailjs/browser"
+var browser_ = __webpack_require__(7163);
+;// CONCATENATED MODULE: ./src/pages/_app.js
+
+/* eslint-disable @next/next/no-before-interactive-script-outside-document */ 
+
+
+
+
+
+function MyApp({ Component , pageProps  }) {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)((head_default()), {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("title", {
+                        children: " Clean Star Energy llc "
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "viewport",
+                        content: "width=device-width, initial-scale=1, maximum-scale=1"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+                ...pageProps
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Progress_Scroll, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx((script_default()), {
+                strategy: "beforeInteractive",
+                src: "/js/bootstrap.bundle.min.js"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((script_default()), {
+                strategy: "beforeInteractive",
+                src: "/js/wow.min.js"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((script_default()), {
+                strategy: "beforeInteractive",
+                src: "/js/splitting.min.js"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((script_default()), {
+                strategy: "beforeInteractive",
+                src: "/js/simpleParallax.min.js"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((script_default()), {
+                strategy: "beforeInteractive",
+                src: "/js/isotope.pkgd.min.js"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((script_default()), {
+                strategy: "beforeInteractive",
+                src: "/landing-preview/js/parallax.min.js"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((script_default()), {
+                strategy: "lazyOnload",
+                src: "/js/main.js"
+            })
+        ]
+    });
+}
+/* harmony default export */ const _app = (MyApp);
+
+
+/***/ }),
+
+/***/ 7163:
+/***/ ((module) => {
+
+module.exports = require("@emailjs/browser");
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 4780:
+/***/ ((module) => {
+
+module.exports = require("next/script");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(1083));
+module.exports = __webpack_exports__;
+
+})();
